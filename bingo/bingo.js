f5fe09203d504f0b0a09be72c1d68c2455067e41
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-01-19 21:56:59 +0100

    modified all errors from ESlint

--- bingo/bingo.js
+++ bingo/bingo.js
@@ -1,229 +1,205 @@
-
-function bingo (){
-
-
-class Jugador {
-    constructor (nombre, puntuacion){
-
-        this.nombre = nombre;
-        this.puntuacion = puntuacion;
+function bingo() {
+  class Jugador {
+    constructor(nombre, puntuacion) {
+      this.nombre = nombre;
+      this.puntuacion = puntuacion;
     }
-};
+  }
 
-let jugador1 = new Jugador("Esteban Quito", 5000);
-let jugador2 = new Jugador("Alan Brito", 200);
-let jugador3 = new Jugador("Aitor Menta", 4500);
-let jugador4 = new Jugador("Elsa Pito", 3000);
-let jugador5 = new Jugador("Ana Conda", 2900);
+  const jugador1 = new Jugador("Esteban Quito", 5000);
+  const jugador2 = new Jugador("Alan Brito", 200);
+  const jugador3 = new Jugador("Aitor Menta", 4500);
+  const jugador4 = new Jugador("Elsa Pito", 3000);
+  const jugador5 = new Jugador("Ana Conda", 2900);
 
-let playerNombre = prompt("Introduce tu nombre de jugador");
+  const playerNombre = prompt("Introduce tu nombre de jugador");
+  const puntuacionInicial = 7000;
 
-var puntuacionInicial = 7000;
+  const ranking = [];
 
+  ranking.push(jugador1);
+  ranking.push(jugador2);
+  ranking.push(jugador3);
+  ranking.push(jugador4);
+  ranking.push(jugador5);
 
+  let cantidadNumeros = 30;
+  let myArray = [];
 
-let ranking = [];
-
-ranking.push(jugador1);
-ranking.push(jugador2);
-ranking.push(jugador3);
-ranking.push(jugador4);
-ranking.push(jugador5);
-
-
-
-
-  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-var cantidadNumeros = 30;
-var myArray = []
-
-function generarNumeroRandom () {
-
-  
-  while(myArray.length < cantidadNumeros ){
-    var numeroRandom = Math.ceil(Math.random()*cantidadNumeros);
-    var existe = false;
-    for(var i=0;i<myArray.length;i++){
-    if(myArray [i] == numeroRandom){
+  function generarNumeroRandom() {
+    while (myArray.length < cantidadNumeros) {
+      const numeroRandom = Math.ceil(Math.random() * cantidadNumeros);
+      let existe = false;
+      for (let i = 0; i < myArray.length; i++) {
+        if (myArray[i] === numeroRandom) {
           existe = true;
           break;
+        }
+      }
+      if (!existe) {
+        myArray[myArray.length] = numeroRandom;
+        console.log(`El número obtenido es ${numeroRandom}`);
+
+        return numeroRandom;
       }
     }
-    if(!existe){
-      myArray[myArray.length] = numeroRandom;
-      console.log(`El número obtenido es ${numeroRandom}`)
-    
-    return numeroRandom;
-    }
-  
+
+    return new Error("error");
   }
 
+  function generarCartonRandom() {
+    cantidadNumeros = 15;
+    myArray = [];
 
-};
-
-
-function generarCartonRandom () {
-
-let cantidadNumeros = 15;
-let myArray = []
-
-while(myArray.length < cantidadNumeros ){
-  let numeroAleatorio = Math.floor(Math.random()*30+1);
-  let existe = false;
-  for(let i=0;i<myArray.length;i++){
-	if(myArray [i] == numeroAleatorio){
-        existe = true;
-        break;
+    while (myArray.length < cantidadNumeros) {
+      const numeroAleatorio = Math.floor(Math.random() * 30 + 1);
+      let existe = false;
+      for (let i = 0; i < myArray.length; i++) {
+        if (myArray[i] === numeroAleatorio) {
+          existe = true;
+          break;
+        }
+      }
+      if (!existe) {
+        myArray[myArray.length] = numeroAleatorio;
+      }
     }
-  }
-  if(!existe){
-    myArray[myArray.length] = numeroAleatorio;
-  };
-};
 
-let linea1 = myArray.slice(0,5);
-let linea2 = myArray.slice(5,10);
-let linea3 = myArray.slice(10,15);
-let carton = [linea1, linea2, linea3];
+    const linea1 = myArray.slice(0, 5);
+    const linea2 = myArray.slice(5, 10);
+    const linea3 = myArray.slice(10, 15);
+    const carton = [linea1, linea2, linea3];
 
-console.table(carton);
+    console.table(carton);
 
-let confirmarCarton = prompt("Te gusta este cartón?", "YES, OTRO");
+    const confirmarCarton = prompt("Te gusta este cartón?", "YES, OTRO");
 
+    if (confirmarCarton.toLowerCase() === "yes") {
+      alert("Perfecto, a jugar!");
+    }
 
-  if (confirmarCarton.toLowerCase() == "yes") {
-  
-  alert("Perfecto, a jugar!");
-
-
-  
-  };
-  
-  if (confirmarCarton.toLowerCase() == "otro") {
-  
-  return generarCartonRandom();
-  
-  };
-  return carton;
-
-};
-
-  var numeroTurnos = [];
-  function turno(){
-
-    let otroTurno = window.confirm("Sacamos un número?");
-
-    if (otroTurno == true) {
-
-      var numeroRandom = generarNumeroRandom();
-     
-      
-      for(i=0; i<carton.length; i++){
-
-        for(j=0; j<carton[i].length; j++){
-    
-          if (carton[i][j] === numeroRandom) {
-    
-            carton[i][j] = "X";
-    
-          };
-    
-        };
-    };
-    numeroTurnos.push(numeroRandom); 
+    if (confirmarCarton.toLowerCase() === "otro") {
+      return generarCartonRandom();
+    }
     return carton;
+  }
 
+  const numeroTurnos = [];
+
+  const carton = generarCartonRandom();
+
+  function turno() {
+    const otroTurno = window.confirm("Sacamos un número?");
+
+    if (otroTurno === true) {
+      const numeroRandom = generarNumeroRandom();
+
+      for (let i = 0; i < carton.length; i++) {
+        for (let j = 0; j < carton[i].length; j++) {
+          if (carton[i][j] === numeroRandom) {
+            carton[i][j] = "X";
+          }
+        }
+      }
+      numeroTurnos.push(numeroRandom);
+      return carton;
     }
 
-    if (otroTurno == false) {
+    if (otroTurno === false) {
+      alert("Ok, hasta la próxima!");
+    }
+    return new Error("error");
+  }
 
-        alert("Ok, hasta la próxima!")
-    }; 
+  for (let i = 0; i < 100; i++) {
+    const cartonChecked = turno();
 
-    
-};
+    console.table(cartonChecked);
 
-var carton = generarCartonRandom();
+    if (
+      cartonChecked[0][0] === "X" &&
+      cartonChecked[0][1] === "X" &&
+      cartonChecked[0][2] === "X" &&
+      cartonChecked[0][3] === "X" &&
+      cartonChecked[0][4] === "X"
+    ) {
+      alert("LINEA!");
 
+      break;
+    } else if (
+      cartonChecked[1][0] === "X" &&
+      cartonChecked[1][1] === "X" &&
+      cartonChecked[1][2] === "X" &&
+      cartonChecked[1][3] === "X" &&
+      cartonChecked[1][4] === "X"
+    ) {
+      alert("LINEA!");
+      break;
+    } else if (
+      cartonChecked[2][0] === "X" &&
+      cartonChecked[2][1] === "X" &&
+      cartonChecked[2][2] === "X" &&
+      cartonChecked[2][3] === "X" &&
+      cartonChecked[2][4] === "X"
+    ) {
+      alert("LINEA!");
+      break;
+    }
+  }
 
-
-for (i=0; i<100; i++){
-var cartonChecked = turno();
-
-console.table(cartonChecked);
-
-if (cartonChecked[0][0] === "X" && cartonChecked[0][1] === "X" && cartonChecked[0][2] === "X" && cartonChecked[0][3] === "X" && cartonChecked[0][4] === "X") {
-  alert("LINEA!")
-
-  break;
-}
-else if (cartonChecked[1][0] === "X" && cartonChecked[1][1] === "X" && cartonChecked[1][2] == "X" && cartonChecked[1][3] === "X" && cartonChecked[1][4] === "X" ) {
-
-  alert("LINEA!")
-  break;
-
-}
-else if (cartonChecked[2][0] === "X" && cartonChecked[2][1] === "X" && cartonChecked[2][2] === "X" && cartonChecked[2][3] === "X" && cartonChecked[2][4] === "X" ) {
-
-  alert("LINEA!")
-  break;
-
-}
-
-
-};
-
-
-for (i=0; i<100; i++){
-
-if (cartonChecked[0][0] == "X" && cartonChecked[0][1] == "X" && cartonChecked[0][2] == "X" && cartonChecked[0][3] == "X" && cartonChecked[0][4] == "X" 
-    && cartonChecked[1][0] == "X" && cartonChecked[1][1] == "X" && cartonChecked[1][2] == "X" && cartonChecked[1][3] == "X" && cartonChecked[1][4] == "X"
-    && cartonChecked[2][0] == "X" && cartonChecked[2][1] == "X" && cartonChecked[2][2] == "X" && cartonChecked[2][3] == "X" && cartonChecked[2][4] == "X") {
-
+  for (let i = 0; i < 100; i++) {
+    let cartonChecked;
+    if (
+      cartonChecked[0][0] === "X" &&
+      cartonChecked[0][1] === "X" &&
+      cartonChecked[0][2] === "X" &&
+      cartonChecked[0][3] === "X" &&
+      cartonChecked[0][4] === "X" &&
+      cartonChecked[1][0] === "X" &&
+      cartonChecked[1][1] === "X" &&
+      cartonChecked[1][2] === "X" &&
+      cartonChecked[1][3] === "X" &&
+      cartonChecked[1][4] === "X" &&
+      cartonChecked[2][0] === "X" &&
+      cartonChecked[2][1] === "X" &&
+      cartonChecked[2][2] === "X" &&
+      cartonChecked[2][3] === "X" &&
+      cartonChecked[2][4] === "X"
+    ) {
       alert("BINGO!");
       break;
+    } else {
+      cartonChecked = turno();
 
-    } 
-    
-
-else  {
-
-var cartonChecked = turno();
-
-console.table(cartonChecked);
-};
-
-};
-
-console.log(`Has completado el cartón en ${numeroTurnos.length} turnos! `);
-var puntuacionFinal = puntuacionInicial - (numeroTurnos.length * 100);
-
-let player = new Jugador (playerNombre, puntuacionFinal)
-
-ranking.push(player);
-
-ranking.sort(function (a, b) {
-  if (a.puntuacion < b.puntuacion) {
-    return 1;
-  }
-  if (a.puntuacion > b.puntuacion) {
-    return -1;
+      console.table(cartonChecked);
+    }
   }
 
-  return 0;
-});
+  console.log(`Has completado el cartón en ${numeroTurnos.length} turnos! `);
+  const puntuacionFinal = puntuacionInicial - numeroTurnos.length * 100;
 
-console.log(`Enhorabuena ${playerNombre}, tu puntuación es ${puntuacionFinal} y éste es ranking actual de jugadores:`)
+  const player = new Jugador(playerNombre, puntuacionFinal);
 
-ranking.forEach(function(jugador){
+  ranking.push(player);
 
-  console.log(`${jugador.nombre} con ${jugador.puntuacion} puntos`);
+  ranking.sort((a, b) => {
+    if (a.puntuacion < b.puntuacion) {
+      return 1;
+    }
+    if (a.puntuacion > b.puntuacion) {
+      return -1;
+    }
 
-});
+    return 0;
+  });
 
+  console.log(
+    `Enhorabuena ${playerNombre}, tu puntuación es ${puntuacionFinal} y éste es ranking actual de jugadores:`
+  );
 
-};
+  ranking.forEach((jugador) => {
+    console.log(`${jugador.nombre} con ${jugador.puntuacion} puntos`);
+  });
+}
 
 bingo();
--- skylinesAirlines/airlines.js
+++ skylinesAirlines/airlines.js
@@ -1,110 +1,89 @@
-
-
 // VUELOS
 
-let flights = [
+const flights = [
+  { id: 0, to: "Bilbao", from: "Barcelona", cost: 1600, scale: false },
 
-    { id: 00, to: 'Bilbao', from: 'Barcelona', cost: 1600, scale: false },
+  { id: 1, to: "New York", from: "Barcelona", cost: 700, scale: false },
 
-    { id: 01, to: 'New York', from: 'Barcelona', cost: 700, scale: false },
+  { id: 2, to: "Los Angeles", from: "Madrid", cost: 1100, scale: true },
 
-    { id: 02, to: 'Los Angeles', from: 'Madrid', cost: 1100, scale: true },
+  { id: 3, to: "Paris", from: "Barcelona", cost: 210, scale: false },
 
-    { id: 03, to: 'Paris', from: 'Barcelona', cost: 210, scale: false },
+  { id: 4, to: "Roma", from: "Barcelona", cost: 150, scale: false },
 
-    { id: 04, to: 'Roma', from: 'Barcelona', cost: 150, scale: false },
+  { id: 5, to: "London", from: "Madrid", cost: 200, scale: false },
 
-    { id: 05, to: 'London', from: 'Madrid', cost: 200, scale: false },
+  { id: 6, to: "Madrid", from: "Barcelona", cost: 90, scale: false },
 
-    { id: 06, to: 'Madrid', from: 'Barcelona', cost: 90, scale: false },
+  { id: 7, to: "Tokyo", from: "Madrid", cost: 1500, scale: true },
 
-    { id: 07, to: 'Tokyo', from: 'Madrid', cost: 1500, scale: true },
+  { id: 8, to: "Shangai", from: "Barcelona", cost: 800, scale: true },
 
-    { id: 08, to: 'Shangai', from: 'Barcelona', cost: 800, scale: true },
+  { id: 9, to: "Sydney", from: "Barcelona", cost: 150, scale: true },
 
-    { id: 09, to: 'Sydney', from: 'Barcelona', cost: 150, scale: true },
-
-    { id: 10, to: 'Tel-Aviv', from: 'Madrid', cost: 150, scale: false } ];
-
+  { id: 10, to: "Tel-Aviv", from: "Madrid", cost: 150, scale: false },
+];
 
 function skylabAirlines() {
+  const nombreUsuario = prompt("Hola, cuál es tu nombre?");
 
-let nombreUsuario = prompt("Hola, cuál es tu nombre?");
+  alert(`Hola ${nombreUsuario}, aquí tienes un listado de los vuelos de hoy:`);
 
-alert(`Hola ${nombreUsuario}, aquí tienes un listado de los vuelos de hoy:`);
+  flights.forEach((vuelo) => {
+    if (vuelo.scale === false) {
+      console.log(
+        `Desde ${vuelo.from} hasta ${vuelo.to} con un precio de ${vuelo.cost}€, sin escala`
+      );
+    }
+    if (vuelo.scale === true) {
+      console.log(
+        `Desde ${vuelo.from} hasta ${vuelo.to} con un precio de ${vuelo.cost}€, con escala`
+      );
+    }
+  });
 
+  const precios = flights.map((vuelo) => vuelo.cost);
 
-flights.forEach(function(vuelo) {
-if (vuelo.scale === false) {
-console.log(`Desde ${vuelo.from} hasta ${vuelo.to} con un precio de ${vuelo.cost}€, sin escala`)
+  // precio medio vuelos
 
-}
-if (vuelo.scale === true) {
-    console.log(`Desde ${vuelo.from} hasta ${vuelo.to} con un precio de ${vuelo.cost}€, con escala`)
-
-}
-});
-
-let precios = flights.map(function (vuelo) {
-    return vuelo.cost;
-});
-
-// precio medio vuelos
-
-function suma(...n) {
-
+  function suma() {
     let sum = 0;
     let i;
-    for(i=0; i<(precios.length); i++) {
-
-        sum += precios[i];
+    for (i = 0; i < precios.length; i++) {
+      sum += precios[i];
     }
-return sum;
+    return sum;
+  }
 
-  };
+  const totalPrecios = suma();
 
+  const mediaPrecios = totalPrecios / precios.length;
 
-let totalPrecios = suma ();
+  console.log(
+    `El precio medio de los vuelos de hoy es ${Math.round(mediaPrecios)}€`
+  );
 
-let mediaPrecios = (totalPrecios / precios.length)
+  // cuantos vuelos hacen escala
 
-console.log(`El precio medio de los vuelos de hoy es ${Math.round(mediaPrecios)}€`);
+  const vuelosEscala = [];
 
-// cuantos vuelos hacen escala
-
-let vuelosEscala = [];
-
-flights.forEach(function(vuelo) {
-    
-    
+  flights.forEach((vuelo) => {
     if (vuelo.scale === true) {
-        vuelosEscala.push(vuelo);
+      vuelosEscala.push(vuelo);
     }
-    });
+  });
 
-    console.log(`Hay ${vuelosEscala.length} vuelos con escala el día de hoy`);
+  console.log(`Hay ${vuelosEscala.length} vuelos con escala el día de hoy`);
 
-    // console.table(flights);
+  // console.table(flights);
 
-    // console.table(flights.to);
+  // console.table(flights.to);
 
-let destinos = flights.map(function (vuelo) {
-
-    return vuelo.to;
-
-
-});
-console.log("Los destinos de los últimos 5 vuelos de hoy son:")
-for (i = destinos.length-1; i>5; i--) {
-    
+  const destinos = flights.map((vuelo) => vuelo.to);
+  console.log("Los destinos de los últimos 5 vuelos de hoy son:");
+  for (let i = destinos.length - 1; i > 5; i--) {
     console.log(destinos[i]);
-
-};
-
-
-};
-
-
+  }
+}
 
 skylabAirlines();
-
